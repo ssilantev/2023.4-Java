52608f103857f50b06a1d141d31801a15b75d963
ssilantev <un.realgar@yandex.ru>
2023-03-10 20:21:21 +0700

    Task "Average" is ready

---
+++ src/ru/academ_it_school/avarage/Average.java
@@ -0,0 +1,44 @@
+package ru.academ_it_school.avarage;
+
+public class Average {
+
+	public static boolean checkInt(String s) {
+		try {
+			int tmp = Integer.parseInt(s);
+			return true;
+		} catch (NumberFormatException e) {
+			System.out.print("Неверный тип данных. ");
+		}
+		return false;
+	}
+
+	public static int strToInt(String s) {
+			return Integer.parseInt(s);
+	}
+
+	public static double getAverage(int aInt, int bInt) {
+
+		int m = 0;
+		double sum = 0.0;
+		while (aInt <= bInt) {
+			sum = sum + aInt;
+			m++;
+			aInt++;
+		}
+		return (sum / m);
+	}
+
+	public static double getAverageEven(int aInt, int bInt) {
+
+		int n = 0;
+		double sumE = 0.0;
+		while (aInt <= bInt) {
+			if (aInt % 2 == 0) {
+				sumE = sumE + aInt;
+				}
+			n++;
+			aInt++;
+		}
+		return (sumE / n);
+	}
+}
--- src/ru/academ_it_school/avarage/Main.java
+++ src/ru/academ_it_school/avarage/Main.java
@@ -1,4 +1,28 @@
 package ru.academ_it_school.avarage;
 
+import java.util.Scanner;
 public class Main {
+	public static void main(String[] args) {
+		System.out.println("Найти среднее арифметическое всех значений от a до b, когда a и b натуральные, a < b и a > 1.");
+		Scanner scanner = new Scanner(System.in);
+
+		String a = "";
+		while ((a.length() == 0) || (!Average.checkInt(a)) || (Average.strToInt(a) < 2))
+		{
+			System.out.print("Введите число a > 1: ");
+			a = scanner.nextLine();
+		}
+		System.out.println("Спасибо!");
+
+		String b = "";
+		while ((b.length() == 0) || (!Average.checkInt(b)) || (Average.strToInt(b) <= Average.strToInt(a)))
+		{
+			System.out.print("Введите число b (b > a): ");
+			b = scanner.nextLine();
+		}
+		System.out.println("Спасибо!");
+
+		System.out.println("Среднее арифметическое всех чисел от a до b равно: " + Average.getAverage(Average.strToInt(a), Average.strToInt(b)));
+		System.out.println("Среднее арифметическое всех четных чисел от a до b равно: " + Average.getAverageEven(Average.strToInt(a), Average.strToInt(b)));
+	}
 }
